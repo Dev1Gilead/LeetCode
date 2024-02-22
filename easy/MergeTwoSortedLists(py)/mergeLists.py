803ef73317ce3390a2ae0e81ce1e956a1798d5f4
Dev1Gilead <gster0909@gmail.com>
2024-02-22 11:58:02 -0800

    prep code for a problem and created file structrue

---
+++ easy/MergeTwoSortedLists(py)/mergeLists.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        for a in range(0,9):
+            print(a)
+            a += 1
+
+for a in range(0,9):
+    print(a)
+    a += 1
